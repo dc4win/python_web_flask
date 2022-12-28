3464a5bed5bc1682d518a3c82966883c5f4ab493
dc4win <dc1014549257dc@gmail.com>
2022-12-28 16:36:55 +0800

    202212281636

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route("/fwb/info")
 def index():
-    return "镇江气象服务中心"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
